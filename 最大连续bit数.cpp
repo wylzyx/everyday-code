57cbaa5e29f480dc22650bf6bffa012aa9d8fa0e
wylzyx <wanyu990124@163.com>
2019-09-12 20:12:29 +0800

    2019-09-12

---
+++ 最大连续bit数.cpp
@@ -0,0 +1,28 @@
+//根据位运算，获取每一位的二进制值。获取第i位的值：(n >> i) & 1。如果1连续，则计数累加，如果不连 续，则从0开始计数。
+#include<iostream> 
+#include<string> 
+#include<algorithm> 
+using namespace std; 
+int main() {
+	int n;
+	while (cin >> n) {
+		int count = 0, maxCount = 0;
+		// n右移32次就变为0        
+		while (n) {
+			//获取当前位的二进制值            
+			if (n & 1) {
+				//如果1的值连续，计数累加，并且跟新最大计数               
+				++count;
+				maxCount = max(count, maxCount);
+			}
+			else {
+				//如果1的值不连续，重新累加                
+				count = 0;
+			}
+			//右移一次，为获取下一位二进制值做准备            
+			n = n >> 1;
+		}
+		cout << maxCount << endl;
+	}
+	return 0;
+}
